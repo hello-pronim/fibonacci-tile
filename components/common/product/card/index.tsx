1d829f69279966ffa4c50cab1f7719619801e6fd
Yalamber Subba <yalamber@mundhum.com>
2021-11-19 15:13:24 +0545

    fixes

--- components/common/product/card/index.tsx
+++ components/common/product/card/index.tsx
@@ -28,10 +28,36 @@ const ProductCard = ({ product, isSelected = false, onProductSelect }) => {
             onMouseEnter={onMouseButtonHover}
             onMouseLeave={onMouseButtonLeave}
           >
-            
+            <svg
+              width="13"
+              height="14"
+              viewBox="0 0 13 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12.8996 7.63961H0.0996094V6.35961H12.8996V7.63961Z"
+                fill="white"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7.13961 0.599609L7.13961 5.07961L5.85961 5.07961L5.85961 0.599609L7.13961 0.599609Z"
+                fill="white"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7.13961 8.91961L7.13961 13.3996H5.85961L5.85961 8.91961L7.13961 8.91961Z"
+                fill="white"
+              />
+            </svg>
+
             {cardSelectionActive && (
               <Text color="white" variant="Body-XSmall">
-                {isSelected ? "Remove Selection": "Add To Selection"}
+                {isSelected ? "Remove Selection" : "Add To Selection"}
               </Text>
             )}
           </button>
