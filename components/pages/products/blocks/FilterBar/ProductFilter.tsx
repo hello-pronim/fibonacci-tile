47e73629ac935b15bbf31d5e2bb678938f1ed1cc
Yalamber Subba <yalamber@mundhum.com>
2021-11-25 08:18:57 +0545

    FixeS

--- components/pages/products/blocks/FilterBar/ProductFilter.tsx
+++ components/pages/products/blocks/FilterBar/ProductFilter.tsx
@@ -11,50 +11,96 @@ export default function ProductFilter({}) {
     dispatch({
       type: "SELECT_PRODUCT_FILTER",
       filter: {
-        type: 'products',
-        value
+        type: "products",
+        value,
       },
     });
-  }
+  };
   return (
-    <div className={classnames(styles.productsFilterContainer, styles.firstItemBorder)}>
-      <div className={classnames(styles.productFilterItem, {[styles.activeFilter] : state.filter.products === "all"})}>
-        <Text onClick={()=> handleFilter('all')} as="h3" variant="Body-Small">
-          All products {state.filter.products === "all" && <CheckMarkIcon color="#141414" />}
-        </Text>
-      </div>
-      <div className={classnames(styles.productFilterItem, {[styles.activeFilter] : state.filter.products === "tiles"})}>
-        <div onClick={()=> handleFilter('tiles')}>
+    <div
+      className={classnames(
+        styles.productsFilterContainer
+      )}
+    >
+      <div
+        className={classnames(styles.productFilterItem, {
+          [styles.activeFilter]: state.filter.products === "all",
+        })}
+      >
+        <div className={styles.filterInner} onClick={() => handleFilter("all")}>
           <Text as="h3" variant="Body-Small">
-            Tiles {state.filter.products === "tiles" && <CheckMarkIcon color="#141414" />}
-          </Text>
-          <Text as="p" variant="Body-Small">
-            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus aenean.
+            All products{" "}
+            {state.filter.products === "all" && (
+              <CheckMarkIcon color="#141414" />
+            )}
           </Text>
         </div>
       </div>
-      <div className={classnames(styles.productFilterItem, {[styles.activeFilter] : state.filter.products === "slabs"})}>
-        <div onClick={()=> handleFilter('slabs')}>
+      <div
+        className={classnames(styles.productFilterItem, {
+          [styles.activeFilter]: state.filter.products === "tiles",
+        })}
+      >
+        <div
+          className={styles.filterInner}
+          onClick={() => handleFilter("tiles")}
+        >
           <Text as="h3" variant="Body-Small">
-            Slabs {state.filter.products === "slabs" && <CheckMarkIcon color="#141414" />}
+            Tiles{" "}
+            {state.filter.products === "tiles" && (
+              <CheckMarkIcon color="#141414" />
+            )}
           </Text>
           <Text as="p" variant="Body-Small">
-            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus aenean.
+            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus
+            aenean.
           </Text>
         </div>
       </div>
-      <div className={classnames(styles.productFilterItem, {[styles.activeFilter] : state.filter.products === "releases"})}>
-        <div onClick={()=> handleFilter('releases')}>
+      <div
+        className={classnames(styles.productFilterItem, {
+          [styles.activeFilter]: state.filter.products === "slabs",
+        })}
+      >
+        <div
+          className={styles.filterInner}
+          onClick={() => handleFilter("slabs")}
+        >
           <Text as="h3" variant="Body-Small">
-            Latest Releases {state.filter.products === "releases" && <CheckMarkIcon color="#141414" />}
+            Slabs{" "}
+            {state.filter.products === "slabs" && (
+              <CheckMarkIcon color="#141414" />
+            )}
           </Text>
           <Text as="p" variant="Body-Small">
-            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus aenean.
+            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus
+            aenean.
           </Text>
         </div>
       </div>
-      <div onClick={()=> handleFilter('all')} className={styles.clearFilter}>
-        Clear Filter <Cross color={"#0D0D0D"}/>
+      <div
+        className={classnames(styles.productFilterItem, {
+          [styles.activeFilter]: state.filter.products === "releases",
+        })}
+      >
+        <div
+          className={styles.filterInner}
+          onClick={() => handleFilter("releases")}
+        >
+          <Text as="h3" variant="Body-Small">
+            Latest Releases{" "}
+            {state.filter.products === "releases" && (
+              <CheckMarkIcon color="#141414" />
+            )}
+          </Text>
+          <Text as="p" variant="Body-Small">
+            Amet orci facilisi magna nunc vel, est leo adipiscing. Rhoncus
+            aenean.
+          </Text>
+        </div>
+      </div>
+      <div onClick={() => handleFilter("all")} className={styles.clearFilter}>
+        Clear Filter <Cross color={"#0D0D0D"} />
       </div>
     </div>
   );
