e4ed322269bee0e8f94f891bcf1f64fcbdf85697
Josh Mapley <joshm@JM-Traffic.local>
2021-12-17 14:50:15 +1100

    product index slider styling adjustments

--- components/pages/products/blocks/Slider/index.tsx
+++ components/pages/products/blocks/Slider/index.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import Slider from "react-slick";
 import Image from "next/image";
 import Slide1 from "public/banner1.jpg";
 import Slide2 from "public/banner2.jpg";
 import styles from "./styles.module.scss";
+import { css } from "@styled-system/css";
 
 function TopSlider() {
   const settings = {
@@ -11,35 +13,43 @@ function TopSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     center: true,
-    // autoplay: true,
-    autoplaySpeed: 6000,
-    speed: 2000,
+    speed: 500,
     arrows: false,
   };
+
+  const slider = React.useRef<Slider>(null);
+
+  const gotoNext = () => {
+    slider.current !== null && slider.current.slickNext();
+  };
+
+  console.log(slider.current);
   return (
-    <section className={styles.container}>
-      <Slider {...settings}>
-        <div className={styles.slideItem}>
+    <section css={css({width: '100%', overflow: 'hidden'})}>
+      <div className={styles.container} css={css({'.slick-list': {overflow: 'visible'}})}>
+      <Slider {...settings} ref={slider}>
+        <div className={styles.slideItem} onClick={() => gotoNext()}>
           <Image src={Slide1} alt="image-1" width={3080} height={1410} layout="responsive" />
           <div className={styles.bannerTextWrapper}>
             <h3 className={styles.bannerSubHeading}>VIEW OUR</h3>
             <h2 className={styles.bannerHeading}>Terrazzo</h2>
             <p className={styles.bannerDescription}>
-              40 unique creations. Thoughtfully designed. Sustainably made. Purpose-built.
+              40 unique creations. Thoughtfully designed. <br/> Sustainably made. Purpose-built.
             </p>
           </div>
         </div>
-        <div className={styles.slideItem}>
+        <div className={styles.slideItem} onClick={() => gotoNext()}>
           <Image src={Slide2} alt="image-2" />
           <div className={styles.bannerTextWrapper}>
             <h3 className={styles.bannerSubHeading}>VIEW OUR</h3>
             <h2 className={styles.bannerHeading}>Collection</h2>
             <p className={styles.bannerDescription}>
-              5 unique collections. Thoughtfully designed. Sustainably made. Purpose-built.
+              5 unique collections. Thoughtfully designed.<br/> Sustainably made. Purpose-built.
             </p>
           </div>
         </div>
       </Slider>
+      </div>
     </section>
   );
 }
