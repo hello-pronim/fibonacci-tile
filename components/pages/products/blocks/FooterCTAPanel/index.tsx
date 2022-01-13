9c83f6d25c0f5ad5f1e92bba1eb05dc0600e8534
Prajwal Subba <prajwalpalungwa@gmail.com>
2022-01-14 00:29:05 +0545

    Product Listing Page

--- components/pages/products/blocks/FooterCTAPanel/index.tsx
+++ components/pages/products/blocks/FooterCTAPanel/index.tsx
@@ -8,19 +8,23 @@ import styles from "./styles.module.scss";
 const FooterCTAPanel = () => {
   return (
     <section className={styles.footerCTAContainer}>
-      <Image src={FooterCTAImage} alt="Footer-CTA-1" /> 
+      <div className={styles.ctaImgBox}>
+        <Image src={FooterCTAImage} alt="Footer-CTA-1" />
+      </div>
       <div className={styles.ctaDetails}>
-        <Text as="h3" variant="Body-XSmall" marginTop="25px" color="white">
-          Didn't find what you were looking for?
-        </Text>
-        <Text as="h2" variant="Display-Large" altFont={true} color="white" marginTop="25px">
-          We offer custom design solutions
-        </Text>
-        <Text as="p" variant="Body-Large" marginBottom="80px" color="white">
-        Get in touch duis ullamcorper dapibus fermentum. Ut viverra at mi, nec. A eget vel imperdiet volutpat lobortis.
-        Hendrerit integer sit tempor. t vel imperdiet volutpat
-        </Text>
-        <Button mode="light" title="Contact us today" link="http://localhost:3000/products" />
+        <div className={styles.ctaDetailsBox}>
+          <Text as="h3" variant="Body-XSmall" marginTop="25px" color="white">
+            Didn't find what you were looking for?
+          </Text>
+          <Text as="h2" variant="Display-Large" altFont={true} color="white" marginTop="25px">
+            We offer custom design solutions
+          </Text>
+          <Text as="p" variant="Body-Large" marginBottom="80px" color="white">
+          Get in touch duis ullamcorper dapibus fermentum. Ut viverra at mi, nec. A eget vel imperdiet volutpat lobortis.
+          Hendrerit integer sit tempor. t vel imperdiet volutpat
+          </Text>
+          <Button mode="light" title="Contact us today" link="http://localhost:3000/products" />
+        </div>
       </div>
     </section>
   );
