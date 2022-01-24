fcafac717b6aacbcc17dfd5e83383fbf0dd7c6ca
Yalamber Subba <yalamber@mundhum.com>
2022-01-24 13:02:32 +0545

    checkout form css changed form scss to txs

--- components/pages/checkout/components/delivery.tsx
+++ components/pages/checkout/components/delivery.tsx
@@ -1,11 +1,23 @@
 import Text from "@componentscommon/typography";
 import { css } from "@styled-system/css";
-import DeliveryForm from "../form/DeliveryForm";
+import { useForm } from 'react-hook-form';
 import {
-    FormWrapper
+    FormWrapper,
+    OneItemRow,
+    TwoItemRow,
+    ThreeItemRow,
+    InfoText,
+    Details,
+    InputSixty,
+    InputThirty,
+    SelectThirty,
+    InputTwentyFour,
+    TextareaFullwidth
 } from "./styles";
 
 const Delivery = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div>
             <Text
@@ -18,7 +30,47 @@ const Delivery = () => {
                 Send my samples to:
             </Text>
             <FormWrapper>
-                <DeliveryForm/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <TwoItemRow>
+                        <InputSixty type="text" placeholder="Address" {...register("Address", {required: true, maxLength: 80})} />
+                        <InputThirty type="text" placeholder="Suburb" {...register("Suburb", {required: true, maxLength: 100})} />
+                    </TwoItemRow>
+                    <ThreeItemRow>
+                        <InputTwentyFour type="text" placeholder="Postcode" {...register("Postcode", {required: true, maxLength: 80})} />
+                        <SelectThirty {...register("State", { required: true })}>
+                            <option value="">State</option>
+                            <option value="interiorDesigner">Interior Designer</option>
+                            <option value="architect">Architect</option>
+                            <option value="builder">Builder</option>
+                            <option value="installer">Installer</option>
+                            <option value="stonemason">Stonemason</option>
+                            <option value="other">Other</option>
+                        </SelectThirty>
+                        <SelectThirty {...register("Country", { required: true })}>
+                            <option value="">Country</option>
+                            <option value="interiorDesigner">Interior Designer</option>
+                            <option value="architect">Architect</option>
+                            <option value="builder">Builder</option>
+                            <option value="installer">Installer</option>
+                            <option value="stonemason">Stonemason</option>
+                            <option value="other">Other</option>
+                        </SelectThirty>
+                    </ThreeItemRow>
+                    <OneItemRow>
+                        <TextareaFullwidth placeholder='Additional Information/Special Requirements' {...register("information", { required: true })}>
+                        </TextareaFullwidth>
+                    </OneItemRow>
+                    <InfoText>
+                       <p><strong>NB:</strong> We can not send samples to PO Boxes.</p>
+                    </InfoText>
+                    <Details>
+                        <p><strong>All Sample Requests will be actioned within 24 hours of receiving all of the required information.</strong></p>
+                        <p>As part of our commitment to the environment and keeping you up-to-date with the latest products 
+                        and colours in our range we do not provide samples for libraries. Instead we encourage our customers 
+                        to view our range and request samples as required, per project and take advantage of our same-day 
+                        dispatch express service.</p>
+                    </Details>
+                </form>
             </FormWrapper>
         </div>
     )
--- components/pages/checkout/components/styles.tsx
+++ components/pages/checkout/components/styles.tsx
@@ -12,7 +12,7 @@ const StepItemWrapper = styled("div")(() =>
     display: "flex",
     alignItems: "center",
     flexDirection: "column",
-    fontSize: "12px",
+    fontSize: 0,
     textTransform: "uppercase",
     position: "relative",
     marginLeft: "40px",
@@ -45,7 +45,7 @@ const StepCount = styled("div")(({...props }: containerProps) =>
     justifyContent: "center",
     alignItems: "center",
     color: props.active === true ? "white" : theme.colors.stoneTints[5],
-    fontSize: "16px",
+    fontSize: 2,
     mb: 2
   })
 );
@@ -86,11 +86,131 @@ const BttnLeftPadding = styled("div")(()=>
 
 const FormWrapper = styled("div")(()=> 
   css({
-    paddingTop: "30px"
+    paddingTop: "30px",
+    maxWidth: "860px",
+    "input[type='text'], input[type='tel'], input[type='email'], textarea, select": {
+      fontSize: 2,
+      padding: "20px 15px",
+      color: "charcoal",
+      fontFamily: 1,
+      "&::-webkit-input-placeholder": {  
+        color: "charcoal"
+      },
+      "&::-moz-placeholder": {  
+        color: "charcoal"
+      },
+      "&:-ms-input-placeholder": { 
+        color: "charcoal"
+      },
+      "&:-moz-placeholder": {  
+        color: "charcoal"
+      }
+    },
+    textarea: {
+      height: "100px"
+    }
   })
 );
 
 
+const OneItemRow = styled("div")(()=> 
+  css({
+    paddingBottom: "10px",
+    width: "100%"
+  })
+);
+
+const TwoItemRow = styled("div")(()=> 
+  css({
+    paddingBottom: "10px",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between"
+  })
+);
+
+const ThreeItemRow = styled("div")(()=> 
+  css({
+    paddingBottom: "10px",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between"
+  })
+);
+
+const InfoText = styled("p")(()=> 
+  css({
+    p: {
+      fontsSize: 2,
+      lineHeight: "22.4px",
+      marginTop: "24px"
+    }
+  })
+);
+
+const Details = styled("div")(()=> 
+  css({
+    background: theme.colors.stoneTints[8],
+    padding: "20px",
+    marginTop: "24px",
+    p: {
+      fontSize: 2,
+      lineHeight: "22.4px",
+      strong: {
+        marginBottom: "20px",
+        display: "block"
+      }
+    }
+  })
+);
+
+const InputFullwidth = styled("input")(()=> 
+  css({
+    width: "95.8%"
+  })
+);
+
+const InputFourtyFive= styled("input")(()=> 
+  css({
+    width: "45%"
+  })
+);
+
+const SelectFourtyFive = styled("select")(()=> 
+  css({
+    width: "45%"
+  })
+);
+
+const InputSixty = styled("input")(()=> 
+  css({
+    width: "60%"
+  })
+);
+
+const InputThirty = styled("input")(()=> 
+  css({
+    width: "30%"
+  })
+);
+
+const InputTwentyFour = styled("input")(()=> 
+  css({
+    width: "24%"
+  })
+);
+
+const SelectThirty = styled("select")(()=> 
+  css({
+    width: "30%"
+  })
+);
+
+const TextareaFullwidth = styled("textarea")(()=> 
+  css({
+    width: "96%"
+  })
+);
 
 export {
     StepItemWrapper,
@@ -99,5 +219,18 @@ export {
     ProductContainer,
     ButtonWrapper,
     BttnLeftPadding,
-    FormWrapper
+    FormWrapper,
+    OneItemRow,
+    TwoItemRow,
+    ThreeItemRow,
+    InfoText,
+    Details,
+    InputFullwidth,
+    InputFourtyFive,
+    SelectFourtyFive,
+    InputSixty,
+    InputThirty,
+    SelectThirty,
+    InputTwentyFour,
+    TextareaFullwidth
   };
