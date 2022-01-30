93bada82dc4343e8a79a0b45e47de5b29456f041
Yalamber Subba <yalamber@mundhum.com>
2022-01-30 10:49:53 +0545

    updates

--- gql/productGQL.tsx
+++ gql/productGQL.tsx
@@ -3,14 +3,13 @@ import { gql } from "@apollo/client";
 export const ProductsQuery = gql `
     query {
         entries(section: "products") {
-        uri
-        ... on products_stories_Entry {
-            id
-            subHeading
-            title
-            slug
-            richText
-        }
+            ... on products_product_Entry {
+                id
+                subHeading
+                title
+                slug
+                richText
+            }
         }
     }
 `;
