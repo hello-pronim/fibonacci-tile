aed813c609b75e84d31193765f287bf8aadce8f8
Yalamber Subba <yalamber@mundhum.com>
2022-02-01 17:40:11 +0545

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import client from "@utils/apolloClient";
+import { ProductsQuery } from "@gql/productGQL";
 import Homepage from "@components/pages/home";
 import Header from "@components/common/header";
 import Footer from "@components/common/footer";
 
-const Home = () => {
+interface HomePageProps {
+  products: any;
+}
+
+const Home: NextPage<HomePageProps> = ({ products }) => {
   return (
     <>
       <Head>
@@ -12,10 +19,24 @@ const Home = () => {
         <meta name="robots" content="index, follow" />
       </Head>
       <Header mode="light" position="absolute" />
-      <Homepage />
+      <Homepage products={products} />
       <Footer />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async function () {
+  const {
+    data: { entries: products },
+  } = await client.query({
+    query: ProductsQuery,
+  });
+  return {
+    props: {
+      products,
+    },
+    revalidate: 500,
+  };
 };
 
 export default Home;
