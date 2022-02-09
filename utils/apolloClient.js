26e171d0a9029e086f9561c0e852ec986c918865
Yalamber Subba <yalamber@mundhum.com>
2022-02-09 14:54:12 +0545

    ssrMode config added for apollo client

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL,
-    cache: new InMemoryCache(),
-})
+  ssrMode: typeof window === "undefined",
+  uri: process.env.NEXT_PUBLIC_API_URL,
+  cache: new InMemoryCache(),
+});
 
 export default apolloClient;
