29ac7ddca9fd1a4662c48dac587c6a4149615d6c
prymskiwen <hello.pronim@gmail.com>
2022-02-10 17:42:59 +1000

    fix #17: add padding to collection detail section

--- components/pages/products/blocks/CollectionItem/index.tsx
+++ components/pages/products/blocks/CollectionItem/index.tsx
@@ -2,53 +2,55 @@ import React from "react";
 import ArrowButton from "@components/common/button/arrowButton";
 import ProductCard from "@components/common/product/card";
 import { useAppContext } from "@contexts/AppContext";
-import { 
-    CollectionWrapper, 
-    CollectionDetailContainer, 
-    CollectionProductContainer, 
-    Title,
-    Details,
-    CollectionDetail
+import {
+  CollectionWrapper,
+  CollectionDetailContainer,
+  CollectionProductContainer,
+  Title,
+  Details,
+  CollectionDetail,
 } from "./styles";
 
 const CollectionsItem = ({ collection, collectionProducts }) => {
   const { state, dispatch } = useAppContext();
   return (
     <CollectionWrapper>
-        <CollectionDetailContainer>
-            <CollectionDetail>
-                <Title>{collection.title}</Title>
-                <Details dangerouslySetInnerHTML={{__html: collection.introduction}}/>
-                <ArrowButton
-                mode="dark"
-                title="Explore Collection"
-                link={`/terrazzo/${collection.slug}`}
-                visibleArrow={true}
-                />
-            </CollectionDetail>
-        </CollectionDetailContainer>
-        <CollectionProductContainer>
-          {collectionProducts?.[collection.slug].map((product) => {
-            return (
-              <ProductCard
+      <CollectionDetailContainer>
+        <CollectionDetail>
+          <Title>{collection.title}</Title>
+          <Details
+            dangerouslySetInnerHTML={{ __html: collection.introduction }}
+          />
+          <ArrowButton
+            mode="dark"
+            title="Explore Collection"
+            link={`/terrazzo/${collection.slug}`}
+            visibleArrow={true}
+          />
+        </CollectionDetail>
+      </CollectionDetailContainer>
+      <CollectionProductContainer>
+        {collectionProducts?.[collection.slug].map((product) => {
+          return (
+            <ProductCard
               displayMode="grid"
               product={product}
               isSelected={
-              state?.selectedProducts.findIndex(
+                state?.selectedProducts.findIndex(
                   (sp) => sp.id === product.id
-              ) !== -1
+                ) !== -1
               }
               toggleProductSelect={() =>
-              dispatch({
+                dispatch({
                   type: "TOGGLE_PRODUCT_SELECTION",
                   product,
-              })
+                })
               }
               key={`product-${product.id}`}
-              />
-            )
-          })}
-        </CollectionProductContainer>
+            />
+          );
+        })}
+      </CollectionProductContainer>
     </CollectionWrapper>
   );
 };
