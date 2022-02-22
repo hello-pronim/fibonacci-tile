7c82323bbdfa23f96ad84dea503b876893dc901d
Yalamber Subba <yalamber@mundhum.com>
2022-02-22 13:34:01 +0545

    add _error page

--- components/pages/generic/Header.tsx
+++ components/pages/generic/Header.tsx
@@ -1,10 +1,19 @@
 import Header from "@components/common/header";
 import { HeaderContainer } from "./styles";
 
-const GenericHeader = ({ mode = "light", notifications = null }) => {
+const GenericHeader = ({
+  mode = "light",
+  notifications = null,
+  disableSelectionCart = false,
+}) => {
   return (
     <HeaderContainer>
-      <Header mode={mode} position="absolute" notifications={notifications} />
+      <Header
+        mode={mode}
+        position="absolute"
+        notifications={notifications}
+        disableSelectionCart={disableSelectionCart}
+      />
     </HeaderContainer>
   );
 };
