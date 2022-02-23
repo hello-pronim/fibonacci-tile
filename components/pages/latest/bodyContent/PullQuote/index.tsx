59037da4ae92d56f62c032fa6b67b626403db683
Josh Mapley <joshm@JM-Traffic.local>
2022-02-23 17:49:06 +1100

    pull quote bg fix

--- components/pages/latest/bodyContent/PullQuote/index.tsx
+++ components/pages/latest/bodyContent/PullQuote/index.tsx
@@ -4,7 +4,7 @@ import { css } from "@styled-system/css";
 
 const PullQuote = ({ quote }) => {
   return (
-    <Container css={css({pb: 120,})}>
+    <Container css={css({pb: 120, bg: '#fffff8'})}>
       <div css={css({gridColumn: "2 / span 7"})}>
         <Text as="h1" variant="Display-Medium" altFont>
           {quote}
