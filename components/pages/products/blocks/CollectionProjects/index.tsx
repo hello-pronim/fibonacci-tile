2fe4c2d3006e8ca7e4d91c9168d1cdf6c9cb0551
Prajwal Subba <prajwalpalungwa@gmail.com>
2022-03-02 01:11:08 +0545

    Collection page responsive

--- components/pages/products/blocks/CollectionProjects/index.tsx
+++ components/pages/products/blocks/CollectionProjects/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Text from "@components/common/typography";
-import AccentText from "@components/common/accentText";
+import AccentText, { AccentTextMobile } from "@components/common/accentText";
 import { css } from "@styled-system/css";
 import theme from "@styles/theme";
 import ProjectCard from "@components/common/project/card";
@@ -10,6 +10,7 @@ const CollectionProjects = ({ projects }) => {
   return (
     <CollectionProjectsContainer>
       <AccentText top={275}>Fibonacci in use</AccentText>
+      <AccentTextMobile css={css({ pb: 80 })}> Fibonacci in use</AccentTextMobile>
       <Text variant="Display-Small" altFont={true}>
         Exercise your democratic right with this one - <br /> you know you can trust it.
       </Text>
@@ -18,13 +19,19 @@ const CollectionProjects = ({ projects }) => {
           display: "flex",
           justifyContent: "space-between",
           mt: 80,
+          flexWrap: "wrap",
         })}
       >
         {projects.map((project) => (
           <div
             key={`project-${project.id}`}
             css={css({
-              width: "48%",
+              width: "100%",
+              marginBottom: "50px",
+              [theme.mediaQueries.smedium]: {
+                width: "48%",
+                marginBottom: "0px",
+              },
             })}
           >
             <ProjectCard key={project.id} project={project} />
--- components/pages/products/blocks/CollectionProjects/styles.tsx
+++ components/pages/products/blocks/CollectionProjects/styles.tsx
@@ -1,13 +1,20 @@
 import css from "@styled-system/css";
 import styled from "@emotion/styled";
+import theme from "@styles/theme";
 
 const CollectionProjectsContainer = styled("div")(() =>
   css({
-    pt: 120,
-    pb: 120,
-    pl: 230,
-    pr: 230,
-    position: "relative"
+    pt: 55,
+    pb: 60,
+    pl: 15,
+    pr: 15,
+    position: "relative",
+    [theme.mediaQueries.smedium]: {
+      pt: 120,
+      pb: 120,
+      pl: 230,
+      pr: 230,
+    },
   })
 );
 
