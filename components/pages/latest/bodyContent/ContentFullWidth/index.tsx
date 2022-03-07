4c0b3c6f659040891a34f99056d454aed8f6bdad
Josh Mapley <joshm@JM-Traffic.local>
2022-03-07 15:42:17 +1100

    v0.1 version of rich text renderer, needs typing work and some cleanup

--- components/pages/latest/bodyContent/ContentFullWidth/index.tsx
+++ components/pages/latest/bodyContent/ContentFullWidth/index.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import css from "@styled-system/css";
 import Container from "@components/common/layout/container";
 import { TextCol } from "../styles";
+import richTextRenderer from "@utils/richTextRenderer";
 
-const ContentFullWidth = ({ content }) => {
-  const {contentText} = content;
+const ContentFullWidth = ({ content }) => {  
+  const { contentText } = content;
+
   return (
-    <Container css={css({ bg: "#FFFFF8" })}>
-      <TextCol dangerouslySetInnerHTML={{ __html: contentText }} />
+    <Container css={css({ bg: "#FFFFF8", py: 120 })}>
+      <TextCol>{richTextRenderer(contentText)}</TextCol>
     </Container>
   );
 };
